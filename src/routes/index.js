62c8ffbef67f558ca3b82f7fff05085b5626c178
oavasquez <oavasquez01@gmail.com>
2022-11-16 21:13:41 -0600

    agregando mysql2 con respuesta si o no

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,46 @@
 import { Router } from "express";
 
-const router= Router();
+import { pool } from "../db/db.js";
+
+const router = Router();
 
 
-router.get('/', (req,res)=>{
+router.get('/', (req, res) => {
 
-    
-    console.log(req.query['text'])
-    res.render('index', {title: 'First WebSite with Node', invitado: req.query['invitado']})
+    const result = pool.query(`CALL invitacion_digitalDB.sp_invitados(2 ,'` + req.query['invitado'] + `','');`, function (error, results) {
+        if (error)  console.log(error);
+        var resultado = Object.assign({}, results[0]);
+        res.render('index', { title: 'First WebSite with Node', invitado: resultado[0].nombre_invitado.toLowerCase(), codigo_invitado: req.query['invitado'] , cantidad_personas: resultado[0].cantidad_personas})
+    });
+
+
 })
+
+
+router.get('/confirmation', (req, res) => {
+
+    const result = pool.query(`CALL invitacion_digitalDB.sp_invitados(1 ,'` + req.query['respuesta'] + `','` + req.query['invitado'] + `');`, function (error, results) {
+        if (error) throw console.log(error);
+        var resultado = Object.assign({}, results[0]);
+        res.render('confirmation', { title: 'First WebSite with Node', mensaje: resultado[0].mensaje.toLowerCase() })
+    });
+
+})
+
+
+router.get('/listconfirmation', (req, res) => {
+
+    const result = pool.query(`CALL invitacion_digitalDB.sp_invitados(3 ,'password1','' );`, function (error, results) {
+
+        if (error) throw console.log(error);
+        var resultado = Object.assign({}, results);
+
+        console.log(resultado[0]);
+        res.render('listconfirmation', { title: 'First WebSite with Node', user_data: resultado[0] })
+    });
+
+})
+
 
 
 

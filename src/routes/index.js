7e13b5abf2787981d5bacbcee24d3b660b9392ae
oavasquez <oavasquez01@gmail.com>
2022-11-16 22:46:49 -0600

    correccion de errores al momento de mostrar datos vacios

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,13 +15,13 @@ router.get('/', (req, res) => {
 
         const result = pool.query(`CALL sp_invitados(2 ,'` + req.query['invitado'] + `','');`, function (error, results) {
             if (error) {
-                res.render('nofound', { title: 'Invitacion Boda Vasquez Acosta' })
+                res.render('nofound', { title: 'Invitacion Boda Vasquez Acosta' , mensaje: 'Lo sentimos tenemos problemas para tu generar tu invitacion' })
             }
 
             console.log(results)
             var resultado = Object.assign({}, results[0]);
             if (results[0].length == 0) {
-                res.render('nofound', { title: 'Invitacion Boda Vasquez Acosta' })
+                res.render('nofound', { title: 'Invitacion Boda Vasquez Acosta', mensaje: 'No se encontraron datos'  })
             }else{
                 res.render('index', { title: 'Invitacion Boda Vasquez Acosta', invitado: resultado[0].nombre_invitado.toLowerCase(), codigo_invitado: req.query['invitado'], cantidad_personas: resultado[0].cantidad_personas })
             }
@@ -37,7 +37,7 @@ router.get('/confirmation', (req, res) => {
         if (error) throw console.log(error);
         var resultado = Object.assign({}, results[0]);
         if (results[0].length==0) {
-            res.render('nofound', { title: 'Invitacion Boda Vasquez Acosta' })
+            res.render('nofound', { title: 'Invitacion Boda Vasquez Acosta', mensaje: 'No se encontraron datos' })
         }else{
             res.render('confirmation', { title: 'Invitacion Boda Vasquez Acosta', mensaje: resultado[0].mensaje.toLowerCase() })
         }
@@ -48,13 +48,17 @@ router.get('/confirmation', (req, res) => {
 
 router.get('/listconfirmation', (req, res) => {
 
-    const result = pool.query(`CALL sp_invitados(3 ,'password1','' );`, function (error, results) {
+    const result = pool.query(`CALL sp_invitados(3 ,'password1','');`, function (error, results) {
 
         if (error) throw console.log(error);
         var resultado = Object.assign({}, results);
 
+        if (results[0].length==0) {
+            res.render('nofound', { title: 'Invitacion Boda Vasquez Acosta', mensaje: 'No se encontraron datos'  })
+        }else{
+            res.render('listconfirmation', { title: 'Invitacion Boda Vasquez Acosta', user_data: resultado[0] })   }
        
-        res.render('listconfirmation', { title: 'Invitacion Boda Vasquez Acosta', user_data: resultado[0] })
+       
     });
 
 })

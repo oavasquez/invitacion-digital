0f057431b4d159073781b111438cb4afe267ae61
oavasquez <oavasquez01@gmail.com>
2022-11-19 16:12:03 -0600

    agrgando cambios en configuracion

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,13 @@
-/*
+
 export const PORT   =   process.env.PORT || 3000
 export const DB_HOST   =   process.env.DB_HOST || 'localhost'
 export const DB_USER   =   process.env.DB_USER || 'root'
 export const DB_PASSWORD   =   process.env.DB_PASSWORD || 'istmania'
 export const DB_NAME  =   process.env.DB_NAME || 'usersdb'
 export const DB_PORT   =   process.env.DB_PORT || 3306
-*/
 
 
+/*
 
 export const PORT   =   process.env.PORT || 3000
 export const DB_HOST   =   process.env.DB_HOST || 'containers-us-west-127.railway.app'
@@ -15,3 +15,4 @@ export const DB_USER   =   process.env.DB_USER || 'root'
 export const DB_PASSWORD   =   process.env.DB_PASSWORD || 'wGhf97k1WWLEpks6m7My'
 export const DB_NAME  =   process.env.DB_NAME || 'railway'
 export const DB_PORT   =   process.env.DB_PORT || 6868
+*/
